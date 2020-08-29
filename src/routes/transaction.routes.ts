d07d23938369a9444b62b1196e4d126e58a18fd5
sugaith <sugaith@gmail.com>
2020-08-29 14:45:02 -0300

    up challenge

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,20 @@
-import { Router } from 'express';
+import {Router} from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+    return response.json({
+      transactions,
+      balance,
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +22,17 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+    return response.json(
+      createTransaction.execute({
+        title,
+        value,
+        type,
+      }),
+    );
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
